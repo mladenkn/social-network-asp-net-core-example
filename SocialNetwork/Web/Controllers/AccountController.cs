4b8e49661e4d355e6bf9daa72a452eff218e59d5
Mladen Knezović <mladen.knezovic@protonmail.com>
2018-04-01 13:39:32 +0200

    added AccountController and its Register action

---
+++ SocialNetwork/Web/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialNetwork.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
