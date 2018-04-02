28b73da2f0b566c90bf14867b77b34862b776d5b
Mladen Knezović <mladen.knezovic@protonmail.com>
2018-04-03 01:25:31 +0200

    no message

--- SocialNetwork.TestingUtilities/TestDataContainer.cs
+++ SocialNetwork.TestingUtilities/TestDataContainer.cs
@@ -21,19 +21,19 @@ namespace SocialNetwork.DevelopmentUtilities
                     .Select(it => new KeyValuePair<string, User>(it.UserName, it))
                     .Let(it => new Dictionary<string, User>(it));
 
-            Posts =
-                new[]
+            Posts = 
+                new []
                 {
-                    (DateTime.Parse("2018-03-18"), Users["Mladen"]),
-                    (DateTime.Parse("2018-03-17"), Users["Mate"]),
-                    (DateTime.Parse("2018-03-10"), Users["Frane"]),
-                    (DateTime.Parse("2018-02-20"), Users["Frane"]),
-                    (DateTime.Parse("2018-02-10"), Users["Mladen"]),
-                    (DateTime.Parse("2018-02-09"), Users["Mate"]),
-                    (DateTime.Parse("2018-01-18"), Users["Ante"]),
-                    (DateTime.Parse("2018-01-17"), Users["Mate"])
+                    (new DateTime(2018, 3, 18), "Mladen"),
+                    (new DateTime(2018, 3, 17), "Frane"),
+                    (new DateTime(2018, 3, 20), "Ante"),
+                    (new DateTime(2018, 2, 10), "Mate"),
+                    (new DateTime(2018, 2, 8), "Mladen"),
+                    (new DateTime(2018, 2, 7), "Frane"),
+                    (new DateTime(2018, 1, 18), "Ante"),
+                    (new DateTime(2018, 1, 17), "Mladen"),
                 }
-                .Select(it => Generator.RandomPost(createdAt: it.Item1, author: it.Item2))
+                .Select(it => Generator.RandomPost(createdAt: it.Item1, author: Users[it.Item2]))
                 .ToList();
         }
     }
