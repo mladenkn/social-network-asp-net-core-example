37097f9c46521b633ce20d169d7029d2147fddc2
Mladen Knezović <mladen.knezovic@protonmail.com>
2018-04-08 16:15:09 +0200

    added PermissionsCalculator and PostActions

---
+++ SocialNetwork.Interfaces/Models/PostActions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SocialNetwork.Interfaces.Models
+{
+    [Flags]
+    public enum PostActions
+    {
+        Like = 1,
+        Dislike = 2,
+        Edit = 4,
+        Delete = 8
+    }
+}
---
+++ SocialNetwork.Interfaces/Services/PermissionsCalculator.cs
@@ -0,0 +1,13 @@
+﻿using SocialNetwork.Interfaces.Models;
+using SocialNetwork.Models;
+
+namespace SocialNetwork.Interfaces.Services
+{
+    public class PermissionsCalculator
+    {
+        public UserPostActions Calculate(string userId, Post post)
+        {
+
+        }
+    }
+}
