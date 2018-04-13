0990553d4eb983031abacc54af081f2f7d0849df
Mladen Knezović <mladen.knezovic@protonmail.com>
2018-04-13 11:27:05 +0200

    updated and reorganized view models

--- SocialNetwork/ViewModels/LoginViewModel.cs
+++ SocialNetwork/ViewModels/LoginViewModel.cs
@@ -1,7 +1,23 @@
-﻿namespace SocialNetwork.Web.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialNetwork.Web.ViewModels
 {
     public class LoginViewModel : BaseViewModel
     {
         public LoginFormViewModel Form { get; set; }
+    }
+
+    public class LoginFormViewModel
+    {
+        [Required]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [MinLength(8)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
     }
 }
--- SocialNetwork/ViewModels/RegisterViewModel.cs
+++ SocialNetwork/ViewModels/RegisterViewModel.cs
@@ -1,7 +1,28 @@
-﻿namespace SocialNetwork.Web.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialNetwork.Web.ViewModels
 {
     public class RegisterViewModel : BaseViewModel
     {
         public RegisterFormViewModel Form { get; set; }
+    }
+
+    public class RegisterFormViewModel
+    {
+        [Required]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+
+        [Required]
+        [MinLength(8)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [MinLength(8)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Repeat password")]
+        [Compare("Password", ErrorMessage = "Password doesn't match.")]
+        public string RepeatedPassword { get; set; }
     }
 }
