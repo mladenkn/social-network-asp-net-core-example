c8a987dc7fc566d3f98c3d15b99b7202fee2acb7
Mladen Knezović <mladen.knezovic@protonmail.com>
2018-04-14 13:09:20 +0200

    updated SocialNetworkDbContex.OnConfiguring

--- SocialNetwork.DAL/SocialNetworkDbContext.cs
+++ SocialNetwork.DAL/SocialNetworkDbContext.cs
@@ -18,7 +18,7 @@ namespace SocialNetwork.DAL
 
             builder.Entity<_Rating>()
                 .Ignore(it => it.RatingType)
-                .HasKey(it => new {it.PostId, it.UserId});
+                .HasKey(it => new {it.PostId, it.UserId, it.RatingType});
 
             builder.Entity<Post>()
                 .Ignore(it => it.LikedBy)
